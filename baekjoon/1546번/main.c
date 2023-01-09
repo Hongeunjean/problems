c1b22c6bd8605f37e433a90cb2e6578e01ee452c
hongeunjean <ej0681@naver.com>
2023-01-09 09:17:30 +0900

    edit baekjoon 1546

--- baekjoon/1546번/main.c
+++ baekjoon/1546번/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 	int sub = 0;
-	float point[100]={0}, mean = 0, sum = 0, max = 0;
+	float* point, mean = 0, sum = 0, max = 0;
 	scanf_s("%d", &sub);
+	point = (float*)calloc(sub, sizeof(float));
 
 	for (int i = 0; i < sub; i++) {
 		scanf_s("%f", &point[i]);
@@ -14,7 +16,8 @@ int main() {
 		sum += point[i];
 	}
 	mean = sum / sub;
-	printf("mean %0.2f", mean);
+	printf("%0.6f", mean);
 
+	free(point);
 	return 0;
 }
